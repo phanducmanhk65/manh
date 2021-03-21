021679ae3ed3b8b7cbd54d575808c3751ba18287
unknown <phanducmanh2812002@gmail.com>
2021-03-21 14:13:21 +0700

    update

--- Gamemanh/threatobject.cpp
+++ Gamemanh/threatobject.cpp
@@ -8,10 +8,14 @@ Threatsobject::Threatsobject()
     x_val_=0.0;
     y_val_=0.0;
     x_pos_=0.0;
-    y_pos=0.0;
+    y_pos_=0.0;
     on_ground_=0;
     come_back_time_=0;
     frame_=0;
+    animation_a_=0;
+    animation_b_=0;
+    input_type_.left_=0;
+    type_move_=STATIC_THREAT;
 }
 Threatsobject::~Threatsobject()
 {
@@ -21,7 +25,7 @@ bool Threatsobject::LoadImg(std::string path,SDL_Renderer* screen)
 {
 
     bool ret= BaseObject::LoadImg(path,screen);
-if(ret==true)
+if(ret)
 {
   width_frame_=rect_.w/THREAT_FRAME_NUM;
   height_frame_=rect_.h;
@@ -94,14 +98,197 @@ void Threatsobject::Show(SDL_Renderer* des)
         }
         SDL_Rect* currentClip =&frame_clip_[frame_];
         SDL_Rect renderQuad={rect_.x,rect_.y,width_frame_,height_frame_};
-        SDL_RenderCopy(des,p_object_,current_clip,&renderQuad);
+        SDL_RenderCopy(des,p_object_,currentClip,&renderQuad);
     }
 }
-void Threatsobject::DoPlayer(Map& gmap)
+void Threatsobject::DoPlayer(Map& gMap)
 {
     if(come_back_time_==0)
     {
         x_val_=0;
         y_val_+=THREAT_GRAVITY_SPEED;
+        if(y_val_>=THREAT_MAX_FALL_SPEED)
+        {
+            y_val_=THREAT_MAX_FALL_SPEED;
+        }
+        if(input_type_.left_==1)
+        {
+            x_val_-=THREAT_SPEED;
+
+        }
+        else if(input_type_.right_==1)
+        {
+            x_val_+=THREAT_SPEED;
+        }
+        CheckToMap(gMap);
+    }
+    else if(come_back_time_>0)
+    {
+        come_back_time_--;
+        if(come_back_time_==0)
+        {
+          InitThreats();
+
+        }
+    }
+}
+
+void Threatsobject::InitThreats()
+{
+
+  x_val_=0;
+            y_val_=0;
+            if(x_pos_>256)
+            {
+                x_pos_-=256;
+                animation_a_-=256;
+                animation_b_-=256;
+            }
+            else
+            {
+                x_pos_=0;
+            }
+            y_pos_=0;
+            come_back_time_=0;
+
+
+}
+void Threatsobject::CheckToMap(Map& map_data)
+{
+    int x1=0;
+    int x2=0;
+
+    int y1=0;
+    int y2=0;
+
+    int height_min=height_frame_ <TITLE_SIZE ? height_frame_: TITLE_SIZE;
+
+    x1=(x_pos_+x_val_)/TITLE_SIZE;
+    x2=(x_pos_+x_val_+width_frame_-1)/TITLE_SIZE;
+
+    y1=(y_pos_)/TITLE_SIZE;
+    y2=(y_pos_+height_min -1)/TITLE_SIZE;
+
+
+    if(x1>=0 && x2<MAX_MAP_X && y1>=0&&y2<MAX_MAP_Y)
+    {
+        if(x_val_ >0)
+        {
+
+            int val1=map_data.title[y1][x2];
+            int val2=map_data.title[y2][x2];
+
+
+
+            if((val1!= BLANK_TILE&&val1!=STATE_MONEY)|| (val2!= BLANK_TILE&&val2!=STATE_MONEY))
+            {
+                x_pos_=x2*TITLE_SIZE;
+                x_pos_-= width_frame_+1;
+                x_val_=0;
+            }
+
+        }
+        else if(x_val_<0)
+        {
+            int val1= map_data.title[y1][x1];
+            int val2=map_data.title[y2][x1];
+            if(val1!= BLANK_TILE&&val1!=STATE_MONEY||val2 != BLANK_TILE&&val2!=STATE_MONEY)
+            {
+                x_pos_=(x1+1)*TITLE_SIZE;
+                x_val_=0;
+            }
+        }
+    }
+
+     int width_min= width_frame_ <TITLE_SIZE ?width_frame_:TITLE_SIZE;
+     x1=(x_pos_)/TITLE_SIZE;
+     x2=(x_pos_+width_min)/TITLE_SIZE;
+
+     y1= (y_pos_+ y_val_)/TITLE_SIZE;
+     y2= (y_pos_+ y_val_ +height_frame_-1)/TITLE_SIZE;
+        if(x1>=0 && x2<MAX_MAP_X && y1>=0&&y2<MAX_MAP_Y)
+        {
+            if(y_val_>0)
+            {
+                 int val1=map_data.title[y2][x1];
+                 int val2=map_data.title[y2][x2];
+
+
+                      if(val1!= BLANK_TILE&&val1!=STATE_MONEY|| val2!= BLANK_TILE&&val2!=STATE_MONEY)
+                  {
+                      y_pos_=y2*TITLE_SIZE;
+                      y_pos_-=(height_frame_+1);
+                      y_val_=0;
+                      on_ground_= true;
+                  }
+
+
+
+            }
+            else if(y_val_<0)
+            {
+                 int val1=map_data.title[y1][x1];
+                 int val2=map_data.title[y1][x2];
+
+
+                      if(val1!= BLANK_TILE&&val1!=STATE_MONEY|| val2!= BLANK_TILE&&val2!=STATE_MONEY)
+                 {
+                     y_pos_=(y1+1)*TITLE_SIZE;
+                     y_val_=0;
+                 }
+
+
+
+            }
+        }
+
+
+        x_pos_+=x_val_;
+        y_pos_+=y_val_;
+        if(x_pos_<0)
+        {
+            x_pos_=0;
+        }
+        else if(x_pos_+width_frame_>map_data.max_x_)
+        {
+            x_pos_=map_data.max_x_-width_frame_-1;
+        }
+        if(y_pos_>map_data.max_y_)
+        {
+            come_back_time_=60;
+        }
+}
+void Threatsobject::ImpMoveType(SDL_Renderer* screen)
+{
+    if(type_move_==STATIC_THREAT)
+    {
+        ;
+    }
+    else
+    {
+        if(on_ground_=true)
+        {
+            if(x_pos_>animation_a_)
+            {
+                input_type_.left_=1;
+                input_type_.right_=0;
+                LoadImg("img//threat_left.png",screen);
+            }
+            else if(x_pos_<animation_b_)
+            {
+                input_type_.left_=0;
+                input_type_.right_=1;
+                LoadImg("img//threat_right.png",screen);
+            }
+
+        }
+        else
+        {
+            if(input_type_.left_=1)
+            {
+                LoadImg("img//threat_left.png",screen);
+            }
+        }
+
     }
 }
--- Gamemanh/threatobject.h
+++ Gamemanh/threatobject.h
@@ -4,35 +4,49 @@
 #include "function.h"
 #include "baseobject.h"
 
+#define THREAT_MAX_FALL_SPEED 10
 #define THREAT_GRAVITY_SPEED 0.8
 #define THREAT_FRAME_NUM 8
-class Threatsobject::public BaseObject
+#define THREAT_SPEED 3
+class Threatsobject:public BaseObject
 {
 public:
     Threatsobject();
     ~Threatsobject();
 
+    enum TypeMove
+    {
+        STATIC_THREAT=0,
+        MOVE_IN_SPACE_THREAT=1,
+    };
     void set_x_val(const float& xVal){x_val_=xVal;}
      void set_y_val(const float& yVal){y_val_=yVal;}
 
      void set_x_pos(const float& xp){x_pos_=xp;}
     void set_y_pos(const float& yp){y_pos_=yp;}
-void SetMapXY(const int mp_x,const int mp_y){map_x_=mp_x;map_y_=mp_y;}
+    float get_x_pos()const {return x_pos_;}
+    float get_y_pos()const {return y_pos_;}
+void SetMapXY(const int& mp_x,const int& mp_y){map_x_=mp_x;map_y_=mp_y;}
 
-void setclips();
+void set_clips();
  bool LoadImg(std::string path,SDL_Renderer* screen);
  void Show(SDL_Renderer* des);
  int get_width_frame()const{return width_frame_;}
  int get_height_frame()const{return height_frame_;}
-void DoPlayer(Map& gmap);
-void CheckToMap(Map& gmap);
+void DoPlayer(Map& gMap);
+void CheckToMap(Map& gMap);
+void InitThreats();
 
+void set_type_move(const& typeMove){type_move_=typeMove;}
+void set_animation_pos(const int& pos_a,const int& pos_b){animation_a_=pos_a,animation_b_=pos_b;}
+void set_input_left(const int& ipleft){input_type_.left_=ipleft;}
+void ImpMoveType(SDL_Renderer* screen);
 
 private:
  int map_x_;
  int map_y_;
  float x_pos_;
- float Y_pos_;
+ float y_pos_;
  float x_val_;
  float y_val_;
  bool on_ground_;
@@ -41,5 +55,13 @@ private:
  int width_frame_;
  int height_frame_;
  int frame_;
+
+ int type_move_;
+
+ int animation_a_;
+ int animation_b_;
+
+ Input input_type_;
+
 };
 #endif // THREATS_OBJECT_H
